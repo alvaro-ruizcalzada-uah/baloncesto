161a9905bc2aa7136ab391e7b04038403790bdc9
alvaro-ruizcalzada-uah <alvaro-ruizcalzada@edu.uah.es>
2024-07-07 12:26:12 +0200

    Add logs

--- src/main/java/Acb.java
+++ src/main/java/Acb.java
@@ -7,18 +7,25 @@ import javax.servlet.http.*;
 
 import model.Jugador;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Acb extends HttpServlet {
 
     private ModeloDatos bd;
+
+    private static final Logger logger = LoggerFactory.getLogger(Acb.class);
 
     @Override
     public void init(ServletConfig cfg) throws ServletException {
         bd = new ModeloDatos();
         bd.abrirConexion();
+        logger.info("Se inicia la conexión con la base de datos.");
     }
 
     private void reiniciarVotacion(HttpServletResponse res) throws IOException{
         bd.reiniciarVotos();
+        logger.info("Se reinicia la votación.");
         res.sendRedirect(res.encodeRedirectURL("VotacionReiniciada.jsp"));
     }
 
@@ -34,6 +41,7 @@ public class Acb extends HttpServlet {
         } else {
             bd.insertarJugador(nombre);
         }
+        logger.info("Se vota a {}", nombre);
         s.setAttribute("nombreCliente", nombreP);
         res.sendRedirect(res.encodeRedirectURL("TablaVotos.jsp"));
     }
@@ -41,6 +49,7 @@ public class Acb extends HttpServlet {
     private void mostrarVotos(HttpServletRequest req, HttpServletResponse res) throws IOException {
         HttpSession s = req.getSession(true);
         List<Jugador> jugadores = bd.obtenerTodosLosJugadores();
+        logger.info("Se muestra las votaciones de los {} jugadores.", jugadores.size());
         s.setAttribute("jugadores", jugadores);
         res.sendRedirect(res.encodeRedirectURL("VerVotos.jsp"));
     }
@@ -48,6 +57,7 @@ public class Acb extends HttpServlet {
     @Override
     public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         String accion = req.getParameter("B1");
+        logger.info("Se procesa la acción {}", accion);
         switch(accion) {
             case "Poner votos a cero": 
                 reiniciarVotacion(res);
