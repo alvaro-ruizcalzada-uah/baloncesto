4524985c6877c1a9e3ae53eac3bd3d2f6206f481
alvaro-ruizcalzada-uah <alvaro-ruizcalzada@edu.uah.es>
2024-07-07 10:55:13 +0200

    PFB_Añadir prueba votar otro jugador

--- src/test/java/PruebasPhantomjsIT.java
+++ src/test/java/PruebasPhantomjsIT.java
@@ -12,8 +12,8 @@ import java.util.List;
 public class PruebasPhantomjsIT {
     private static WebDriver driver = null;
 
-@Test
-public void tituloIndexTest() {
+    @Test
+    void tituloIndexTest() {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setJavascriptEnabled(true);
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"/usr/bin/phantomjs");
@@ -25,8 +25,8 @@ public void tituloIndexTest() {
         driver.quit();
     }
 
-@Test
-public void reiniciarVotosTest() {
+    @Test
+    void reiniciarVotosTest() {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setJavascriptEnabled(true);
         caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"/usr/bin/phantomjs");
@@ -47,6 +47,36 @@ public void reiniciarVotosTest() {
             String valorVotos = columnas.get(2).getText();
             assertEquals("0", valorVotos);
         }
+
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+     void votorOtroJugadorTest() {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setJavascriptEnabled(true);
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,"/usr/bin/phantomjs");
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, new String[] {"--web-security=no", "--ignore-ssl-errors=yes"});
+        driver = new PhantomJSDriver(caps);
+
+        driver.navigate().to("http://localhost:8080/Baloncesto/");
+        driver.findElement(By.id("Otros")).click();
+        WebElement txtOtros = driver.findElement(By.id("txtOtros"));
+        txtOtros.clear();
+        String otroJugador = "Otro jugador";
+        txtOtros.sendKeys(otroJugador);
+        driver.findElement(By.id("votar")).click();
+
+        driver.navigate().to("http://localhost:8080/Baloncesto/");
+        driver.findElement(By.id("verVotos")).click();
+        
+        WebElement tablaVotos = driver.findElement(By.id("tablaVotos"));
+        List<WebElement> filas = tablaVotos.findElements(By.tagName("tr"));
+        WebElement ultimaFila = filas.get(filas.size() - 1);
+        List<WebElement> columnas = ultimaFila.findElements(By.tagName("td"));
+        String nombreJugador = columnas.get(1).getText();
+        assertEquals(otroJugador, nombreJugador);
 
         driver.close();
         driver.quit();
